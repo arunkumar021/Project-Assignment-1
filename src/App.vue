037ecd979a119e7110402f3e6f361a3a0e964634
Arunkumar D <99382863+arunkumar021@users.noreply.github.com>
2022-03-03 21:02:20 -0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <navbar-component/>
+  <router-view/>
+</template>
+
+<script>
+import NavbarComponent from './components/NavbarComponent.vue'
+  export default {
+     components: {
+       NavbarComponent,
+     },
+  }
+</script>
+
+<style>
+
+</style>
