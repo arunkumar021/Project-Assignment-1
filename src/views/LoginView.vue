db83f33c8e0ebd99791f49c4a634885008ae289c
Arunkumar D <99382863+arunkumar021@users.noreply.github.com>
2022-03-07 05:08:50 -0800

    Update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,34 +1,35 @@
-<template>
- <div class="row">
-   <div class="col-md-6 offset-md-3" >
-       <div>
+  <template>
     <div>
-      <h3 class=" font-weight-bold d-flex justify-content-center">Login</h3>
-      <hr>
+        <div class="row ">
+            <div class="col-md-6 offset-md-3">
+              <div>
+                <div>
+                    <h3 class=" font-weight-bold d-flex justify-content-center">Login</h3>
+                    <hr>
+                </div>
+                <form class="p-2 bg-light border bg-info text-dark bg-opacity-10"  @submit.prevent = "login">
+
+                    <!-- <div  class="alert alert-success" role="alert">
+                        {{message}}
+                    </div> -->
+
+                <div class="form-group">
+                  <label>Username</label>
+                    <input type="text" class="form-control" v-model=" userData.username"/>
+                </div>
+                <div class="form-group ">
+                  <label>Password</label>
+                  <input type="password" class="form-control" v-model="userData.password" />
+                </div>
+                <div class="my-3 form-group d-flex justify-content-center">
+                    <button type="submit" class="btn btn-primary" @click="login" >Login</button>
+                </div>
+                </form>
+            </div>
+        </div>
+      </div>
     </div>
-    <form @submit.prevent = "login">
-
-      <!-- <div  class="alert alert-success" role="alert">
-          {{message}}
-      </div> -->
-
-      <div class="form-group">
-        <label>Username</label>
-        <input type="text" class="form-control" v-model=" userData.username"/>
-      </div>
-      <div class="form-group">
-        <label>Password</label>
-        <input type="password" class="form-control" v-model="userData.password" />
-      </div>
-      <div class="my-3 form-group d-flex justify-content-center">
-        <button type="submit" class="btn btn-primary" @click="login" >Login</button>
-      </div>
-    </form>
-  </div>
-   </div>
- </div>
-
-</template>
+  </template>
 
 <script>
 import axios from 'axios'
@@ -36,10 +37,8 @@ export default {
     data() {
       return {
         userData : {
-         username: "",
-         password: "",
-         message: "",
-         isLog: false,
+        username: "",
+        password: "",
         },
       }
     },
@@ -48,17 +47,13 @@ export default {
         // console.log(this.userData)
         let result = await axios.post("http://localhost:3000/login" , this.userData)
         console.log(result.data);
-        if(result.data === true) {
-          this.$router.push('/dashboard');
+        if(result.data ) {  //=== true
+          this.$store.commit('setAuthentication' , result.data)
+          // let res = await axios.get("http://localhost:3000");
+          this.$router.push('/dashboard/' + result.data); 
         }
-        else {
-          this.message = "error"
-          isLogged(result);
-        }
-        
       }
     },
-   
 }
 </script>
 
