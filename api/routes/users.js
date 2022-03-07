fd7994d1df22e775a458686e7dbe664ee985ccb4
Arunkumar D <99382863+arunkumar021@users.noreply.github.com>
2022-03-07 05:42:25 -0800

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {login ,  createUser, getUsers } = require('../controllers/users');
+const {login ,  createUser, getUsers ,updateTodo } = require('../controllers/users');
 
   router.post('/login' , login);
   
-  router.get('/' , getUsers)
+  router.get('/:id' , getUsers);
 
   router.post('/' , createUser);
+
+  router.patch('/:id' , updateTodo);
   
 module.exports = router;
