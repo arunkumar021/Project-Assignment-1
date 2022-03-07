d4d74c1dbe9d12e5a6f13681444a81c443b24976
Arunkumar D <99382863+arunkumar021@users.noreply.github.com>
2022-03-07 05:39:11 -0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,14 @@
-
+import { createStore } from 'vuex';
+import axios from 'axios';
+export default createStore({
+        state: {
+            authenticated: false,
+            isLogged: false
+        },
+        mutations: {
+            setAuthentication(state , status) {
+                state.authenticated = status
+            }
+        },
+     
+    })
